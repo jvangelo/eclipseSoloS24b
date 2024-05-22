1c11e6f6e2c38271462b86cf2ed7b5d1db4c23fc
jtvangeloven <26tamia@gmail.com>
2024-05-21 22:19:22 -0400

    Programmer 1 added text

--- src/eclipseSoloS24b/HelloWorld.java
+++ src/eclipseSoloS24b/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github project");
 		System.out.println("Second line of information");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this. Programmer 1 did this.");
 	}
 
 }
